871e7bef3d40aa951b1036197e3c73f8486ec137
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2021-07-20 01:11:33 +0530

    Add files via upload

---
+++ yolo/yolov5/yolo_28.cmd
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l walltime=23:59:00
+#PBS -q gpuq
+#PBS -e errorfile_28.err
+#PBS -o logfile_28.log
+#PBS -l select=1:ncpus=2:ngpus=1
+conda init bash
+module load anaconda3_2020
+conda activate ganu
+~/.conda/envs/ganu/bin/python3 '/lfs/usrhome/btech/ed16b016/scratch/project/yolo/yolov5/train_28.py'
+conda deactivate
---
+++ yolo/yolov5/yolo_33.cmd
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l walltime=23:59:00
+#PBS -q gpuq
+#PBS -e errorfile_33.err
+#PBS -o logfile_33.log
+#PBS -l select=1:ncpus=2:ngpus=1
+conda init bash
+module load anaconda3_2020
+conda activate ganu
+~/.conda/envs/ganu/bin/python3 '/lfs/usrhome/btech/ed16b016/scratch/project/yolo/yolov5/train_33.py'
+conda deactivate
