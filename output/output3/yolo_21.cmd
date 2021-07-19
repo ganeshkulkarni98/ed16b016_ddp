70384f18bb0bec8e49dd294021ac3e1e7edef4f5
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2021-07-20 01:32:32 +0530

    Add files via upload

---
+++ output/output3/yolo_21.cmd
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l walltime=23:59:00
+#PBS -q gpuq
+#PBS -e errorfile_21.err
+#PBS -o logfile_21.log
+#PBS -l select=1:ncpus=2:ngpus=1
+conda init bash
+module load anaconda3_2020
+conda activate ganu
+~/.conda/envs/ganu/bin/python3 '/lfs/usrhome/btech/ed16b016/scratch/project/yolov5/train_21.py'
+conda deactivate
