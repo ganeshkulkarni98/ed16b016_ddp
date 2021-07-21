a8a9b41b27b4eef17cec20527f6227a915dba4dc
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2021-07-21 21:25:59 +0530

    Delete kfold_yolo/part_ivan directory

--- kfold_yolo/part_ivan/preproc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kaggle datasets download ivanpan/vinbigdata-weights
-unzip vinbigdata-weights.zip -d weights/
-
-kaggle datasets download ivanpan/vinbigdata-data
-unzip vinbigdata-data.zip
